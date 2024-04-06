21c8dd6a4fb7cd65de76cda906bf76ca007e38c8
dhanushl0l <dhanushdjtherock@gmail.com>
2024-04-07 02:41:41 +0530

    fix

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
-@app.route('/')
-def hello_world():
-    return "Hello World!"
-if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+from app import app
+
+if __name__ == "__main__":
+    app.run()
